486f5dae76fc4a68559f3fd3dea25da48287f29e
bernazyzz <zebernardomoniz@gmail.com>
2023-07-16 10:24:40 +0100

    exame 18-19

--- 1-ano/PF/Testes e Exames/2018-2019/exame_2018-19.hs
+++ 1-ano/PF/Testes e Exames/2018-2019/exame_2018-19.hs
@@ -1,5 +1,6 @@
 import Data.Char
 import Data.List 
+
 -- Exame 2018/2019
 
 {- 1. Apresente uma definição recursiva das seguintes funções sobre listas:
@@ -8,32 +9,32 @@ import Data.List
 isSorted' :: (Ord a) => [a] -> Bool
 isSorted' [] = True 
 isSorted' [x] = True 
-isSorted' (x:y:t) = if x < y then isSorted' (y:t) else False 
+isSorted' (x:xs:t) = if x < xs then isSorted' (xs:t)
+                     else False 
 
 {- (b) inits :: [a] -> [[a]] que calcula a lista dos prefixos de uma lista. Por exemplo, inits [11,21,13]
 corresponde a [[],[11],[11,21],[11,21,13]]. -}
 
 inits' :: [a] -> [[a]]
 inits' [] = [[]]
-inits' l = inits' (init l ) ++ [l]
+inits' l = inits'(init l) ++ [l]
 
 {- 2. Defina maximumMB :: (Ord a) => [Maybe a] -> Maybe a que dá o maior elemento de uma lista de
 elementos do tipo Maybe a. Considere Nothing o menor dos elementos. -}
 
 maximumMB :: (Ord a) => [Maybe a] -> Maybe a
 maximumMB [Just x] = Just x
-maximumMB ((Nothing):t) = maximumMB t
-maximumMB (a:(Nothing):t) = maximumMB (a:t)
-maximumMB ((Just x):(Just y):t) | x >= y = maximumMB ((Just x):t)
-                                | otherwise = maximumMB((Just y):t)
+maximumMB (Nothing:t) = maximumMB t
+maximumMB (a:Nothing:t) = maximumMB (a:t)
+maximumMB ((Just x):(Just y):t) | x >= y = maximumMB (Just x:t)
+                                | otherwise = maximumMB(Just y:t)
 maximumMB _ = Nothing 
 
 --3. Considere o seguinte tipo para representar árvores em que a informação está nas extermidades:
 
 data LTree a = Tip a | Fork (LTree a) (LTree a)
 
-{- (a) Defina a função listaLT :: LTree a -> [a] que dá a lista das folhas de uma árvore (da esquerda
-para a direita). -}
+{- (a) Defina a função listaLT :: LTree a -> [a] que dá a lista das folhas de uma árvore (da esquerda para a direita). -}
 
 listaLT :: LTree a -> [a]
 listaLT (Tip x) = [x]
@@ -51,8 +52,7 @@ instance Show a => Show (LTree a) where
     show (Tip x) = show x
     show x = show' x 0
              where show' (Tip a) c = replicate c '.' ++ show a
-                   show' (Fork e d) c = show' e (c+1) ++ '\n':show' d (c+1)
-
+                   show' (Fork ae ad) c = show' ae (c+1) ++ '\n':show' ad (c+1)
 
 {- 4. Utilizando uma função auxiliar com acumuladores, optimize a seguinte definição que determina a soma
 do segmento inicial de uma lista com soma máxima.  -}
@@ -61,8 +61,7 @@ maxSumInit' :: (Num a, Ord a) => [a] -> a
 maxSumInit' l = maximum [sum m | m <- inits l]
 
 maxSumInit'' :: (Num a, Ord a) => [a] -> a
-maxSumInit'' l = foldl (\ acc x -> max (sum x) acc ) (sum l) (inits l)
-
+maxSumInit'' l = foldl (\ acc x -> max(sum x) acc) (sum l) (inits l)
 
 {- 5. Uma relação binária entre elementos de um tipo a pode ser descrita como um conjunto (lista) de pares
 [(a,a)]. Outras formas alternativas consistem em armazenar estes pares agrupados de acordo com a sua
@@ -81,15 +80,25 @@ f 1 = [3,4], f 2 = [1,4,5], f 3 = [7], f 4 = [7], f 5 = [7], f 6 = [5], e f 7 = 
 
 convLP :: RelL a -> RelP a
 convLP l = concat (map junta l)
-           where junta (x,xs) = map (\y->(x,y)) xs
+           where junta (x,xs) = map (\y -> (x,y)) xs
 
 {- Defina a função de conversão convPL :: (Eq a) => RelP a -> RelL a, inversa da anterior. Isto
 é, tal que convPL (convLP r) = r, para todo o r. -}
 
 convPL :: (Eq a) => RelP a -> RelL a
-conPL [(x,y)] = [(x,[y])]
+convPL [(x,y)] = [(x,[y])]
 convPL (h:t) = junta h (convPL t)
                where junta (a,b) l = if a `elem` map (fst) l then map (\(c,d) -> if c == a then (c,b:d) else (c,d)) l else (a,[b]):l
+
+--ou
+
+convPL' :: (Eq a,Ord a) => RelP a -> RelL a
+convPL' l = let a = sortOn fst l
+                b = groupBy (\(x,_) (y,_) -> x == y) a
+                k = map junta b
+                junta :: [(a,a)] -> (a,[a])
+                junta x = (fst (head x),(map snd x))
+            in k
 
 --(b) Defina a função criaRelPint :: Int -> IO (RelP Int), tal que criaRelPint n permite ao utilizador criar (interactivamente) uma relação de inteiros com n pares.
 {-
