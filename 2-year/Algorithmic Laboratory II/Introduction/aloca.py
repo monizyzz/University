ff9281a31a5b087a21c1c16e978f54bacd17cd3a
bernazyzz <zebernardomoniz@gmail.com>
2024-02-15 10:24:46 +0000

    Organisation + First Java Program

---
+++ 2-year/Algorithmic Laboratory II/Introduction/aloca.py
@@ -0,0 +1,11 @@
+"""
+
+Implemente uma função que dado um dicionário com as preferências dos alunos
+por projectos (para cada aluno uma lista de identificadores de projecto, 
+por ordem de preferência), aloca esses alunos aos projectos. A alocação
+é feita por ordem de número de aluno, e cada projecto só pode ser feito
+por um aluno. A função deve devolver a lista com os alunos que não ficaram
+alocados a nenhum projecto, ordenada por ordem de número de aluno.
+
+"""
+
