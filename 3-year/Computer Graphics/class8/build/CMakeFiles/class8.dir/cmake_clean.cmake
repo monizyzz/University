b6850b095271986d65c1f561e2ecd73f72331821
bernazyzz <zebernardomoniz@gmail.com>
2025-03-26 12:56:20 +0000

    curves

---
+++ 3-year/Computer Graphics/class8/build/CMakeFiles/class8.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/class8.dir/main.cpp.o"
+  "CMakeFiles/class8.dir/main.cpp.o.d"
+  "class8"
+  "class8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/class8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
