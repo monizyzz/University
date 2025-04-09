267bf6d4e1b93262bd1e95af8379b6b9dc566dd6
bernazyzz <zebernardomoniz@gmail.com>
2025-04-09 12:27:13 +0100

    picking snowman

---
+++ 3-year/Computer Graphics/class12/build/CMakeFiles/class12.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/class12.dir/main.cpp.o"
+  "CMakeFiles/class12.dir/main.cpp.o.d"
+  "class12"
+  "class12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/class12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
